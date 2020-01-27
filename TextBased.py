45a15f793006a916fd6d7680e999fb51686286b6
Ben0623 <benjamin.volino1@marist.edu>
2020-01-27 18:57:17 -0500

    First save

---
+++ TextBased.py
@@ -0,0 +1,7 @@
+#Name: Text Based Game
+#Creator: Ben Volino
+#Date: 1/27/20
+def main():
+    print ("you are going to make a PB and J sandwich. to win the \n game you must make the sandwich correctly")
+main()
+
