67c74e422afd7e1f8ed9f21be5d6446e6776c22c
Ben0623 <benjamin.volino1@marist.edu>
2020-02-03 18:11:21 -0500

    Mad-Libs

---
+++ madlib.py
@@ -0,0 +1,8 @@
+# Learning how to use vars
+
+noun = input ("Enter a noun")
+verb = input ("Enter a Verb")
+adj = input ("Enter an adjective")
+place = input ("Enter a place")
+phrase = ("Take your", adj, noun,"and", verb, "it at the", place,"!")
+print (phrase)
